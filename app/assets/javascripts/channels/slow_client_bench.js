3d9433eebbc1fb7e47558955fe4ca931e0b3c2ba
Tinco Andringa <mail@tinco.nl>
2016-06-28 02:06:25 +0200

    render delay metrics

--- app/assets/javascripts/channels/slow_client_bench.js
+++ app/assets/javascripts/channels/slow_client_bench.js
@@ -3,20 +3,27 @@
 //= require_tree .
 
 this.App = {
-  last_delay: 0
+  last_delay: 0,
+  current_delay: 0
 };
 
 App.cable = ActionCable.createConsumer();
 
-App.timestamps= App.cable.subscriptions.create('SlowClientBenchmarkChannel', {
+App.timestamps = App.cable.subscriptions.create('SlowClientBenchmarkChannel', {
   received: function(data) {
     if (data.time) {
-      var now = Math.round(new Date());
-      var then = Math.round(data.time*1000.0);
-      console.log("Difference", now - then);
+      this.processTimeMessage(data);
     }
   },
-
-  renderMessage: function(data) {
+  processTimeMessage: function(data) {
+    this.last_delay = this.current_delay;
+    var now = Math.round(new Date());
+    var then = Math.round(data.time*1000.0);
+    this.current_delay = now - then;
+    this.renderTimeMessage();
+  },
+  renderTimeMessage: function() {
+    $('#delay').text(this.last_delay);
+    $('#delay_change').text(Math.abs(this.last_delay - this.current_delay));
   }
 });
